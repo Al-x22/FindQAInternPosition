af031cfb81652a33e98e0ceecb47d276b1d8105c
Alejandro Ballesteros Perez <aballe@ttu.ee>
2023-03-21 18:51:07 +0200

    TASK-3; Verify if Text is shown on page

--- src/main/java/QABrowserAutomation/Main.java
+++ src/main/java/QABrowserAutomation/Main.java
@@ -1,6 +1,7 @@
 package QABrowserAutomation;
 
 import QABrowserAutomation.Service.WebPage;
+import org.openqa.selenium.WebElement;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,6 +9,10 @@ public class Main {
         WebPage webPage = new WebPage();
         webPage.openWebPage("https://playtech.ee/");
         webPage.findLinkInPage("Internship").click();
+
+        String elementToFind = webPage.findElementInPage("Development QA Engineer (Intern)");
+        System.out.println(elementToFind);
+
         webPage.quitWebPage();
 
     }
--- src/main/java/QABrowserAutomation/Service/WebPage.java
+++ src/main/java/QABrowserAutomation/Service/WebPage.java
@@ -1,9 +1,14 @@
 package QABrowserAutomation.Service;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class WebPage {
 
@@ -19,5 +24,31 @@ public class WebPage {
     }
     public WebElement findLinkInPage (String element){
         return driver.findElement(By.xpath("//a[text()='" + element + "']"));
+    }
+    public String findElementInPage (String sentenceToSearch){
+
+        try
+        {
+            // By waiting until h3 is loaded, I can assure that the whole html has loaded, and then being able
+            // to get the whole body properly
+           WebElement waitUntilHeaderLoads = new WebDriverWait(driver, Duration.ofSeconds(10))
+                  .until(driver -> driver.findElement(By.tagName("h3")));
+
+            WebElement body = driver.findElement(By.tagName("body"));
+            String bodyText = body.getText();
+
+            String[] paragraphs = bodyText.split("\\n\\s*\\n"); // split the body text into paragraphs
+
+            for (String paragraph : paragraphs) {
+                if (paragraph.contains(sentenceToSearch)) {
+                    return "The following text contains '" + sentenceToSearch + "':\n\n" + paragraph;
+                }
+                else return "'" + sentenceToSearch + "' is not shown on the page.";
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return "There is an error with the element you want to search in the page. Please try again.";
     }
 }
