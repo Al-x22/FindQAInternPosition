3dd500b5d333b7e0289ae93467864af473edfd99
Alejandro Ballesteros Perez <aballe@ttu.ee>
2023-03-22 16:33:15 +0200

    REV; Tests Refactor

--- src/test/java/QABrowserAutomation/MainTest.java
+++ src/test/java/QABrowserAutomation/MainTest.java
@@ -1,18 +1,18 @@
 package QABrowserAutomation;
 
 import QABrowserAutomation.Service.WebPage;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static QABrowserAutomation.Main.writeResultToTxt;
-import static org.assertj.core.api.Assertions.assertThat;
 
 class MainTest {
 
     WebPage webPage;
 
     @BeforeEach
-    public void initializeGreeter() {
+    public void initializeWebPage() {
         webPage = new WebPage();
     }
 
@@ -23,8 +23,7 @@ class MainTest {
         webPage.openWebPage("https://playtech.ee/");
         webPage.findLinkInPage("Internship").click();
         String actualOutput = webPage.findElementInPage("Development QA Engineer (Intern)");
-        webPage.quitWebPage();
-        writeResultToTxt(actualOutput);
+        System.out.println(actualOutput);
 
     }
 
@@ -36,7 +35,12 @@ class MainTest {
         int[] coordinates = webPage.getLinkCoordinates("Internship");
         webPage.clickLinkInPageUsingCoordinates(coordinates[0], coordinates[1]);
         String actualOutput = webPage.findElementInPage("Development QA Engineer (Intern)");
-        webPage.quitWebPage();
         writeResultToTxt(actualOutput);
     }
+
+    @AfterEach
+    public void closingWebPage() {
+        webPage.quitWebPage();
+    }
+
 }
